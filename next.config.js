6439fcf09c8e40ccc077a3388172d5b9f9963088
Sikal <sikal.sikal.ss@gmail.com>
2021-11-10 12:10:25 +0100

    env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    MONGODB_URI: "mongodb+srv://Sikal:sikal777@myshop.crv0h.mongodb.net/myshop?retryWrites=true&w=majority",
-    ACTIVATION_TOKEN_KEY:"h3h229dwddsikal",
-    ACTIVATION_TOKEN_EXPIRY:"30d"
-  }
 }
